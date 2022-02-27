a2a8d188b4fbde1b33f88611e265042137505f0b
taplahylje <98735727+taplahylje@users.noreply.github.com>
2022-02-27 17:55:25 +0200

    sex

---
+++ index.test.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+describe(' Program ', () => {
+    
+    it('can take input', () => {
+        
+        const { songs } = require('./songs.json');
+        console.log(songs)
+    })
+    
+    it.todo('can sort items based on genre')
+    
+    it.todo('can suggest songs based on a quiz')
+    
+})
